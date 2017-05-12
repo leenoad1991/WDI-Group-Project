1dc919a0ac5d140c5719120de86740eb8fa81fe0
loyd0 <samloyd@hotmail.co.uk>
2017-05-12 15:56:17 +0100

    added Index stuff

--- config/env.js
+++ config/env.js
@@ -1,1 +1,9 @@
 //getting to grip with Git!
+module.exports = {
+  env: process.env.NODE_ENV,
+  db: {
+    production: process.env.MONGODB_URI,
+    development: `mongodb://localhost/appointments-${this.env}`,
+    test: `mongodb://localhost/appointments-${this.env}`
+  }
+};
--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const port    = process.env.PORT || 4000;
 const app     = express();
 const dest    = `${__dirname}/public`;
 
+//Installed Dependencies
+const mongoose   = require('mongoose');
+mongoose.Promise = require('bluebird');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+const expressJWT = require('express-jwt');
+const env        = require('./config/env');
+const routes     = require('./config/routes');
+
+//db connection
+mongoose.connect(env.db[process.env.NODE_ENV]);
+
+//middlewear
+app.user(morgan('dev'));
+
+if (app.get('env') !== 'production') app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(dest));
 
+//Insert JWT exceptions upon completion of user routes and database
+//JWT Error handler review here
+
+app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
 app.listen(port, () => console.log(`Express has started on port: ${port}`));
