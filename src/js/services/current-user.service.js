93596642401a4f78e3cf9c4773e98648e9d2ed59
seangantly <gantly.sean@gmail.com>
2017-05-13 14:50:19 +0100

    conflict resolution on user service

--- src/js/services/current-user.service.js
+++ src/js/services/current-user.service.js
@@ -2,9 +2,8 @@ angular
 .module('wineApp')
 .service('CurrentUserService', CurrentUserService);
 
-<<<<<<< HEAD
-CurrentUserService.$inject = ['TokenService', '$rootscope', 'UserFactory'];
-function CurrentUserService(TokenService, $rootscope, UserFactory) {
+CurrentUserService.$inject = ['TokenService', '$rootScope', 'UserFactory'];
+function CurrentUserService(TokenService, $rootScope, UserFactory) {
   const self = this;
   self.getUser = () => {
     const decoded = TokenService.decodeToken();
@@ -15,37 +14,13 @@ function CurrentUserService(TokenService, $rootscope, UserFactory) {
       .then(user => {
         console.log(user, 'User returned');
         self.currentUser = user;
-        $rootscope.$broadcast('loggedIn');
+        $rootScope.$broadcast('LoggedIn');
       });
     }
   };
   self.removeUser = () => {
     self.currentUser = null;
     TokenService.removeToken();
-    $rootscope.$broadcast('loggedOut');
+    $rootScope.$broadcast('LoggedOut');
   };
 }
-=======
-  CurrentUserService.$inject = ['TokenService', '$rootScope', 'UserFactory'];
-  function CurrentUserService(TokenService, $rootScope, UserFactory) {
-    const self = this;
-    self.getUser = () => {
-      const decoded = TokenService.decodeToken();
-      if (decoded) {
-        UserFactory
-        .get({id: decoded.id})
-        .$promise
-        .then(user => {
-          console.log(user, 'User returned');
-          self.currentUser = user;
-          $rootScope.$broadcast('LoggedIn');
-        });
-      }
-    };
-    self.removeUser = () => {
-      self.currentUser = null;
-      TokenService.removeToken();
-      $rootScope.$broadcast('LoggedOut');
-    };
-  }
->>>>>>> 05f074327277aa32df0a658a7b51ac99a9722fc7
