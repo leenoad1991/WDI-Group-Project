e60d7664914403df3fb9e6f56f5321060627f211
leenoad1991 <leenoad1991>
2017-05-13 11:29:16 +0100

    login controller

---
+++ src/js/controllers/login.controller.js
@@ -0,0 +1,20 @@
+angular
+  .module('homeworkApp')
+  .controler('LoginCtrl', LoginCtrl);
+
+LoginCtrl.$inject = ['User', 'CurrentUserService', '$state'];
+function LoginCtrl(User, CurrentUserService, $state) {
+  const vm = this;
+
+  vm.login = () => {
+    User.login(vm.user)
+    .$promise
+    .then(() => {
+      CurrentUserService.getUser();
+      console.log(CurrentUserService.currentUser, '*** current user');
+      $state.go('usersIndex');
+    }, err => {
+      console.log(err, 'error in login controller');
+    });
+  };
+}  
