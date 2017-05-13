568240b19e732b93eead2fce2d63de41e0302274
seangantly <gantly.sean@gmail.com>
2017-05-13 12:40:45 +0100

    updating products.js ctrl

--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,28 @@
-// const express = require('express');
-// const router  = express.Router();
-//
-// const users   = require('../controllers/users');
-// const authentication   = require('../controllers/authentications');
-// const products   = require('../controllers/products');
-//
-// router.route('/register')
-//   .post(authentication.register);
-// router.route('/login')
-//   .post(authentication.login);
-//
-// router.route('/users')
-//   .get(users.index);
-// router.route('/users/:id')
-//   .get(users.show)
-//   .put(users.update)
-//   .delete(users.delete);
-//
-// // products routes
-// router.route('/wine')
-//   .get(products.index);
-// router.route('/wine/:id')
-//   .get(products.show)
-//   .put(products.update);
-//   // .get(product.show) //not sure if we are doing a delete;
-//
-// module.exports = router;
+const express = require('express');
+const router  = express.Router();
+
+const users   = require('../controllers/users');
+const authentication   = require('../controllers/authentications');
+const products   = require('../controllers/products');
+
+router.route('/register')
+  .post(authentication.register);
+router.route('/login')
+  .post(authentication.login);
+
+router.route('/users')
+  .get(users.index);
+router.route('/users/:id')
+  .get(users.show)
+  .put(users.update)
+  .delete(users.delete);
+
+// products routes
+router.route('/wine')
+  .get(products.index);
+router.route('/wine/:id')
+  .get(products.show)
+  .put(products.update);
+  // .get(product.show) //not sure if we are doing a delete;
+
+module.exports = router;
--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,17 @@
-//just added the routes to exports
-module.exports = {
-  show: productsShow,
-  index: productsIndex,
-  update: productsUpdate
+const Product =  require('../models/product');
+
+function productsIndex(req, res) {
+  Product.find((err, products) => {
+    if (err) res.status(500).json({ message: 'Something went wrong finding all products on the server side.'});
+    console.log('hitting productIndex', products);
+    return res.status(200).json(products);
+  });
 }
+
+
+
+module.exports = {
+  // show: productsShow,
+  index: productsIndex
+  // update: productsUpdate
+};
