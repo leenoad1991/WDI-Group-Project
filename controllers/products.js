8e0fc3b11cce4c4ccc3de4dbe53701997898a50f
seangantly <gantly.sean@gmail.com>
2017-05-13 12:47:22 +0100

    sorting out files

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,17 @@
-const Product =  require('../models/product');
-
-function productsIndex(req, res) {
-  Product.find((err, products) => {
-    if (err) res.status(500).json({ message: 'Something went wrong finding all products on the server side.'});
-    console.log('hitting productIndex', products);
-    return res.status(200).json(products);
-  });
-}
-
-
-
-module.exports = {
-  // show: productsShow,
-  index: productsIndex
-  // update: productsUpdate
-};
+// const Product =  require('../models/product');
+//
+// function productsIndex(req, res) {
+//   Product.find((err, products) => {
+//     if (err) res.status(500).json({ message: 'Something went wrong finding all products on the server side.'});
+//     console.log('hitting productIndex', products);
+//     return res.status(200).json(products);
+//   });
+// }
+//
+//
+//
+// module.exports = {
+//   // show: productsShow,
+//   index: productsIndex
+//   // update: productsUpdate
+// };
