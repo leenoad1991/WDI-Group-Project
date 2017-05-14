50dc383790f0e1adab73dd743703f9caba6ae008
loyd0 <samloyd@hotmail.co.uk>
2017-05-14 16:27:43 +0100

    added function to let users be updated by admints

--- src/js/controllers/users/users-index.controller.js
+++ src/js/controllers/users/users-index.controller.js
@@ -11,4 +11,17 @@ function UsersIndexCtrl(UserFactory) {
     vm.users = users;
   }).catch(err => console.log(err, 'error in user index controller'));
   vm.loggedIn = true;
+  vm.update = userUpdate;
+
+  function userUpdate($index) {
+    console.log('update firing');
+    console.log(vm.users[$index]);
+    UserFactory
+      .update({ id: vm.users[$index]._id },
+      vm.users[$index])
+      .$promise
+      .then(() => {
+        console.log('users updated');
+      });
+  }
 }
