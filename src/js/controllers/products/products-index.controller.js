4d04a130c77dbbf9bf944fb53ca9022b694734c5
seangantly <gantly.sean@gmail.com>
2017-05-15 13:28:07 +0100

    adding watch list icon

--- src/js/controllers/products/products-index.controller.js
+++ src/js/controllers/products/products-index.controller.js
@@ -2,12 +2,22 @@ angular
 .module('wineApp')
 .controller('ProductsIndexCtrl', ProductsIndexCtrl);
 
-ProductsIndexCtrl.$inject = ['Product'];
-function ProductsIndexCtrl(Product){
+ProductsIndexCtrl.$inject = ['Product', '$stateParams', 'CurrentUserService'];
+function ProductsIndexCtrl(Product, $stateParams, CurrentUserService){
   const vm = this;
   vm.products = [];
+  const user = CurrentUserService;
+
+  vm.watchClick = watchClick;
 
   productsIndex();
+
+  function watchClick(id){
+    console.log(id);
+    console.log(user.currentUser._id);
+  }
+
+
 
   function productsIndex() {
     vm.products = Product.query();
--- src/js/controllers/products/products-show.controller.js
+++ src/js/controllers/products/products-show.controller.js
@@ -2,12 +2,26 @@ angular
   .module('wineApp')
   .controller('ProductsShowCtrl', ProductsShowCtrl);
 
-ProductsShowCtrl.$inject = ['Product', '$stateParams'];
-function ProductsShowCtrl(Product, $stateParams) {
+ProductsShowCtrl.$inject = ['Product', '$stateParams', '$state'];
+function ProductsShowCtrl(Product, $stateParams, $state) {
   console.log('hitting ProductsShowCtrl');
   console.log($stateParams.id);
   const vm = this;
 
   vm.product = Product.get($stateParams);
   console.log(vm.product);
+
+  vm.delete = productsDelete;
+
+  function productsDelete(){
+    console.log('deleting');
+    console.log($stateParams.id);
+
+    Product
+    .remove({ id: $stateParams.id })
+    .$promise
+    .then(() => {
+      $state.go('productsIndex');
+    });
+  }
 }
