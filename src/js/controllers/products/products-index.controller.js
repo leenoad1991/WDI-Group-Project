57876206c49e35bca7a218df10e2d51c94d51bf0
leenoad1991 <leenoad1991>
2017-05-15 13:11:37 +0100

    working on filters

--- src/js/controllers/products/products-index.controller.js
+++ src/js/controllers/products/products-index.controller.js
@@ -2,10 +2,30 @@ angular
 .module('wineApp')
 .controller('ProductsIndexCtrl', ProductsIndexCtrl);
 
-ProductsIndexCtrl.$inject = ['Product'];
-function ProductsIndexCtrl(Product){
+ProductsIndexCtrl.$inject = ['Product', 'filterFilter', '$scope'];
+function ProductsIndexCtrl(Product, filterFilter, $scope){
   const vm = this;
-  vm.products = [];
+  vm.all = Product.query();
+  vm.minPriceRange = 10;
+  vm.maxPriceRange = 85;
+
+  vm.filtered = vm.products;
+
+  function filterProduct() {
+    const params = { name: vm.q };
+    params.info = { type: vm.type };
+    // params.price = { retail: vm.maxPriceRange };
+    params.info = { year: vm.year };
+    // console.log(params.price.retail);
+    vm.filtered = filterFilter(vm.all, params);
+  }
+
+  filterProduct();
+
+  $scope.$watch(() => vm.q, filterProduct);
+  $scope.$watch(() => vm.type, filterProduct);
+  // $scope.$watch(() => vm.maxPriceRange, filterProduct);
+  $scope.$watch(() => vm.year, filterProduct);
 
   productsIndex();
 
@@ -14,3 +34,7 @@ function ProductsIndexCtrl(Product){
     console.log('running productsIndexCtrl', vm.products);
   }
 }
+
+
+//drop down type.
+//location search
