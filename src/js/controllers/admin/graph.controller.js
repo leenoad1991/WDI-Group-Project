453c1793052556db4c22e426f14813c000319ba1
seangantly <gantly.sean@gmail.com>
2017-05-18 14:35:33 +0100

    adding (almost) working angular-charts. awaiting reformat of upstream data

--- src/js/controllers/admin/graph.controller.js
+++ src/js/controllers/admin/graph.controller.js
@@ -1,24 +1,35 @@
 angular
 .module('wineApp')
-.controller('LineCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-
-  var d = new Date();
-  var n = d.getHours();
-  $scope.labels = [24-n, n];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    // [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-
+.controller('LineCtrl', ['$scope', '$timeout', function ($scope) {
+  $scope.labels = ['0:00am', '2:00am', '4:00am','6:00am', '8:00am', '10:00am', '12:00am'];
+  $scope.series = ['Price'];
+  $scope.data = [];
+  // $scope.onClick = function (points) {
+  //   console.log(points);
+  // };
+  // $scope.options = {
+  //   scales: {
+  //     yAxes: [
+  //       {
+  //         id: 'y-axis-1',
+  //         type: 'linear',
+  //         display: true,
+  //         position: 'left'
+  //       },
+  //       {
+  //         id: 'y-axis-2',
+  //         type: 'linear',
+  //         display: true,
+  //         position: 'right'
+  //       }
+  //     ]
+  //   }
+  // };
   // Simulate async data update
-  $timeout(function () {
-    $scope.data = [
-      // [28, 48, 40, 19, 86, 27, 90],
-      [65, 59, 80, 81, 56, 55, 40]
-    ];
-  }, 3000);
+  // $timeout(function () {
+  //   $scope.data = [
+  //     // [28, 48, 40, 19, 86, 27, 90],
+  //     [65, 59, 80, 81, 56, 55, 40]
+  //   ];
+  // }, 3000);
 }]);
